1658dd4304261b6df7cde3187afda283ea88e28c
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-23 20:48:50 +0300

    HomeWork9

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userMessage;
+        string wordForExit;
+        string password = "exit";
+        bool isWorking = true;
+
+        Console.Write("Введите сообщение для повторения: ");
+        userMessage = Console.ReadLine();
+
+        while (isWorking)
+        {
+            if (isWorking = true)
+            {
+                Console.WriteLine(userMessage);
+            }
+            else
+            {
+                Console.Write($"Введите слово exit для выхода: ");
+                wordForExit = Console.ReadLine();
+
+                if (wordForExit == password)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
